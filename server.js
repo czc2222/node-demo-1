044c7031bc6150928d21045e72361b1008ca8cae
czc222 <1263291450@qq.com>
2021-08-08 15:00:43 +0800

    update 2.0

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ var server = http.createServer(function(request, response){
     </body>
     </html>`)
     response.end()
-  } else if(path === '/x'){
+  } else if(path === '/server'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
     response.write(`h1{color: red;}`)

6d3ee9dca06a3032f93b471aec11a0a980e13a58
czc222 <1263291450@qq.com>
2021-08-08 14:57:30 +0800

    update

--- server.js
+++ server.js
@@ -24,7 +24,19 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你好`)
+    response.write(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>你好</h1>
+        
+    </body>
+    </html>`)
     response.end()
   } else if(path === '/x'){
     response.statusCode = 200
